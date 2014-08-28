97fb0ebee6289ea229d8dfc60ce65e33e4794f03
Hiroaki Nakamura <hnakamur@gmail.com>
2014-08-28 17:46:25 +0900

    Create requirements.txt for installing ansible roles

--- setup02.sh
+++ setup02.sh
@@ -1,14 +1,3 @@
 #!/bin/bash -x
-ansible-galaxy install --roles=. hnakamur.osx-defaults --force
-ansible-galaxy install --roles=. hnakamur.homebrew-packages --force
-ansible-galaxy install --roles=. hnakamur.homebrew-cask-packages --force
-ansible-galaxy install --roles=. hnakamur.osx-login-shell --force
-ansible-galaxy install --roles=. hnakamur.oh-my-zsh --force
-ansible-galaxy install --roles=. hnakamur.homebrew-path --force
-ansible-galaxy install --roles=. hnakamur.osx-go --force
-ansible-galaxy install --roles=. hnakamur.osx-vim --force
-ansible-galaxy install --roles=. hnakamur.osx-tmux --force
-ansible-galaxy install --roles=. hnakamur.osx-direnv --force
-ansible-galaxy install --roles=. hnakamur.osx-ruby --force
-ansible-galaxy install --roles=. hnakamur.osx-node-js --force
+ansible-galaxy install --roles=. -r requirements.txt --force
 ansible-playbook localhost.yml
