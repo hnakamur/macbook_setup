448bcc6a124a2d5cd5db388bffc748b97ca7ff3d
Hiroaki Nakamura <hnakamur@gmail.com>
2014-12-03 12:12:51 +0900

    Use vcs_info for zsh prompt to show svn info as well as git

--- hnakamur.zsh/files/prompt.zsh
+++ hnakamur.zsh/files/prompt.zsh
@@ -1,3 +1,13 @@
-setopt PROMPT_SUBST
-PROMPT='[%D{%Y-%m-%d %H:%M:%S} %~ %? $(__git_ps1 "%s")]
+# Hiroaki Nakamura's zsh prompt config.
+# This config is based on http://mollifier.hatenablog.com/entry/20090814/p1
+# Thanks!
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' formats '(%s)-[%b]'
+zstyle ':vcs_info:*' actionformats '(%s)-[%b|%a]'
+precmd () {
+    psvar=()
+    LANG=en_US.UTF-8 vcs_info
+    [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
+}
+PROMPT='[%D{%Y-%m-%d %H:%M:%S} %~ %? %1(v|%1v|)]
 %# '
